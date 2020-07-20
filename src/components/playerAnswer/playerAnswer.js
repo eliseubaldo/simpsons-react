1bd3f5bf84b5bca4360f07e1f62b51f8162ee50d
Eliseu Baldo <eliseu.baldo@gmail.com>
2020-07-20 09:13:51 -0400

    quiz engine

--- src/components/playerAnswer/playerAnswer.js
+++ src/components/playerAnswer/playerAnswer.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import './playerAnswer.scss';
 
-function PlayerAnswer() {
+function PlayerAnswer({answerQuestion, selectedAnswer, clickable}) {
+
+    const [answerClass, setAnswerClass] = useState(selectedAnswer);
+
+    //let answerClass = selectedAnswer;
+    console.log('answerClass:',answerClass);
+
+    const handleClickA = () => {
+        if (clickable && answerQuestion) {
+            setAnswerClass('answer1');
+            answerQuestion('answer1');
+        }
+    }
+
+    const handleClickB = () => {
+        if (clickable && answerQuestion) {
+            setAnswerClass('answer2');
+            answerQuestion('answer2');
+        }
+    }
+
+    useEffect(() => { 
+        console.log("component updated");
+        setAnswerClass(selectedAnswer);
+    }, [selectedAnswer]);
+
+
     return (
         <div className="PlayerAnswer">
-            <button type="button" className="btn btn-answer">1</button>
-            <button type="button" className="btn btn-answer">2</button>
+            <button type="button" className={`btn btn-answer ${answerClass === 'answer1' ? "active" : ""}`} onClick={handleClickA}>A</button>
+            <button type="button" className={`btn btn-answer ${answerClass === 'answer2' ? "active" : ""}`} onClick={handleClickB}>B</button>
         </div>
     );
 }
--- src/containers/quiz/quiz.js
+++ src/containers/quiz/quiz.js
@@ -1,44 +1,89 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Question from '../../components/question/question';
 import Player from '../../components/player/player';
 import PlayerAnswer from '../../components/playerAnswer/playerAnswer';
+import Loaderspinner from '../../components/loaderspinner/LoaderSpinner';
 import './quiz.css';
 
 function Quiz() {
 
-    let loading = true;
+    const baseURL = 'https://marvelheroes-1d22.restdb.io/rest/simpsons-trivia';
+    
+    const [questions, setQuestions] = useState([{
+        question: null,
+        answer1: null,
+        answer2: null,
+        correctAnswer: null
+    }]);
 
-    const headers = {
-        'x-apikey': '5c3f94b966292476821ca01e',
-        'content-type': 'application/json',
-      };
-      const baseURL = 'https://marvelheroes-1d22.restdb.io/rest/simpsons-trivia';
-    
-      const [questions, setQuestions] = useState([{}]);
-    
-      async function fetchQuestions() {
-          const apiRes = await fetch(baseURL, {headers: headers});
-          return apiRes.json();
-    
-      }; 
-    
-      fetchQuestions().then(res => {
-        setQuestions(res);
-        console.log('terminou');
-        loading = false;
+    const [loading, setLoading] = useState(true);
 
-      });
+    const [quizScore, setQuizScore] = useState({
+        player: 0,
+        computer: 0
+    });
 
+    let [computerAnswer, setComputerAnswer] = useState('oque');
 
+    let [questionCounter, setQuestionCounter] = useState(0);
     
+    useEffect(() => {
+        const headers = {
+            'x-apikey': '5c3f94b966292476821ca01e',
+            'content-type': 'application/json',
+        };
+        fetch(baseURL, {headers: headers})
+        .then((res) => res.json())
+        .then((data) => {
+            setQuestions(data);
+            setLoading(false);
+            console.log(data);
+            
+        })
+    }, []);
+
+    function answerQuestion(answer) {
+
+        if(answer === questions[questionCounter].correctAnswer) {
+            console.log('correct');
+        } else {
+            console.log('wrong');
+        }
+
+        chooseComputerAnswer();
+    }
+
+    function chooseComputerAnswer() {
+        const rnd = Math.floor(Math.random() * 2);
+        console.log('result:', rnd);
+
+        switch (rnd) {
+            case 0:
+                setComputerAnswer('answer1');
+                console.log('curr comp answer1: ', computerAnswer);
+            break;
+            case 1:
+                setComputerAnswer('answer2');
+                console.log('curr comp answer2: ', computerAnswer);
+            break;
+        
+            default:
+                break;
+        }
+
+        setQuestionCounter(questionCounter += 1);
+    }
+
+
+
     return (
         <div className='Quiz'>
-            
-                <section className="bigQuestion">
+            { loading ? <Loaderspinner></Loaderspinner> : null }
+                <section className="bigQuestion"> 
                     <div className="container">
                         <div className="row">
                             <div className="col-12">
-                                <Question question={questions[0]}></Question>
+                                { loading ? null : <Question question={questions[questionCounter]}></Question> }
                             </div>
                         </div>
                     </div>
@@ -53,7 +98,7 @@ function Quiz() {
                                         <Player></Player>
                                     </div>
                                     <div className="col-md-6">
-                                        <PlayerAnswer></PlayerAnswer>
+                                        <PlayerAnswer selectedAnswer={computerAnswer} clickable={false}></PlayerAnswer>
                                     </div>
 
                                 </div>
@@ -62,7 +107,7 @@ function Quiz() {
                             <div className="col-md-6 col-sm-6 col-xs-6">
                                 <div className="row">
                                     <div className="col-md-6">
-                                        <PlayerAnswer></PlayerAnswer>
+                                        <PlayerAnswer answerQuestion={answerQuestion} selectedAnswer={''} clickable={true}></PlayerAnswer>
                                     </div>
 
                                     <div className="col-md-6">
